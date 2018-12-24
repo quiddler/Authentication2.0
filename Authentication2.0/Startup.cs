eb295021c8c494f137069151785551db2c3697cc
quiddler <elioteasterling@gmail.com>
2018-12-24 13:54:05 -0500

    init

--- Authentication2.0/Startup.cs
+++ Authentication2.0/Startup.cs
@@ -15,6 +15,8 @@ namespace Authentication2._0
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +27,7 @@ namespace Authentication2._0
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseIdentityServer();
         }
     }
 }
